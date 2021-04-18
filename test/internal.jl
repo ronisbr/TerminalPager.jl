b887c0a4b461bee2cb6a5572b9b3bc275f5847b2
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-04-18 20:54:54 -0300

    :rotating_light: Add tests of _pager_keyprocess

--- test/internal.jl
+++ test/internal.jl
@@ -60,3 +60,160 @@
     @test lines_cropped == 87
     @test columns_cropped == 51
 end
+
+@testset "Key processing" begin
+    # Down
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :down, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (11, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 0, 20, 20)
+    @test ret == (10, 10, false)
+
+    # Shift down
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :down, value = "", shift = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (15, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 2, 20, 20)
+    @test ret == (12, 10, true)
+
+    # Up
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :up, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (9, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 1, 10, 20, 20, 20)
+    @test ret == (1, 10, false)
+
+    # Shift up
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :up, value = "", shift = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (5, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 2, 10, 0, 20, 20)
+    @test ret == (1, 10, true)
+
+    # Right
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :right, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (10, 11, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 0, 20)
+    @test ret == (10, 10, false)
+
+    # Alt right
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :right, value = "", alt = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (10, 30, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 0, 20)
+    @test ret == (10, 10, false)
+
+    # Shift right
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :right, value = "", shift = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (10, 20, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 2, 20)
+    @test ret == (10, 12, true)
+
+    # Left
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :left, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (10, 9, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 1, 20, 20, 20)
+    @test ret == (10, 1, false)
+
+    # Alt left
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :left, value = "", alt = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (10, 1, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 1, 20, 20, 20)
+    @test ret == (10, 1, false)
+
+    # Shift left
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :left, value = "", shift = true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 15, 20, 20, 20)
+    @test ret == (10, 5, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 4, 20, 2, 20)
+    @test ret == (10, 1, true)
+
+    # End
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :end, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (30, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 0, 20, 20)
+    @test ret == (10, 10, false)
+
+    # Home
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :home, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 20, 20, 20)
+    @test ret == (1, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 1, 10, 0, 20, 20)
+    @test ret == (1, 10, false)
+
+    # Page up
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :pageup, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 40, 10, 20, 20, 20)
+    @test ret == (20, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 0, 20, 20)
+    @test ret == (1, 10, true)
+
+    # Page down
+    # ==========================================================================
+
+    k = TerminalPager.Keystroke(ktype = :pagedown, value = "")
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 30, 20, 20)
+    @test ret == (30, 10, true)
+
+    ret = TerminalPager._pager_keyprocess(k, 10, 10, 10, 20, 20)
+    @test ret == (20, 10, true)
+
+end
