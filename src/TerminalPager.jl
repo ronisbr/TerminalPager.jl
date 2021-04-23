a77f82f83816dc8ebd97a4b83bbc3434b6e9da29
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-04-23 20:59:42 -0300

    :bookmark: Version 0.1.0

--- src/TerminalPager.jl
+++ src/TerminalPager.jl
@@ -19,7 +19,7 @@ include("./types.jl")
 ################################################################################
 
 const CSI = "\x1b["
-const PKG_VERSION = v"0.0.1"
+const PKG_VERSION = v"0.1.0"
 const _default_keybindings = Dict{Tuple{Union{Symbol, String}, Bool, Bool, Bool}, Symbol}()
 const _default_search_highlighting = Dict{Bool, Decoration}()
 
