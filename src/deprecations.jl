9d86da978e3832dba89a819a441a88810950e0ee
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-03-13 21:05:47 -0300

    :poop: Remove deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -6,12 +6,3 @@
 #   Deprecated functions.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-#                       Introduced in TerminalPager v0.1
-# ==============================================================================
-
-export @dpr
-macro dpr(expr)
-    Base.depwarn("@dpr is deprecated, use @help instead.", :dpr)
-    return :(@help $(esc(expr)))
-end
